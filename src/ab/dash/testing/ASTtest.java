ea7222697cbcce21b1c7355cfd8761f1a8800827
Mark Galloway <mgallowa@ualberta.ca>
2014-10-17 14:04:12 -0600

    Automated Testing Framework for AST

--- src/ab/dash/testing/ASTtest.java
+++ src/ab/dash/testing/ASTtest.java
@@ -3,17 +3,13 @@ package ab.dash.testing;
 import static org.junit.Assert.*;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.io.File;
 import java.io.PrintStream;
 
 import org.antlr.runtime.RecognitionException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import ab.dash.DashAB_Part1_Test;
-import ab.dash.exceptions.InvalidAssignmentException;
-import ab.dash.exceptions.ParserException;
 
 public class ASTtest {
 
@@ -34,17 +30,60 @@ public class ASTtest {
     public void tearDown() throws Exception {
         System.setOut(out_backup);
         System.setErr(err_backup);
-        SampleFileWriter.destroy("Tests/00dummytest.dash");
+        SampleFileWriter.destroy("Tests/00dummytest.db");
     }
     
     @Test // Dummy Test
-    public void dummyTest() throws IOException, RecognitionException, ParserException, InvalidAssignmentException {
-        SampleFileWriter.createFile("Tests/00dummytest.dash", "");
+    public void dummyTest() throws RecognitionException {
+        SampleFileWriter.createFile("Tests/00dummytest.db", "");
         
-        String[] args = new String[] {"Tests/00dummytest.dash","astDebug"};
-        DashAB_Part1_Test.main(args);
+        String[] args = new String[] {"Tests/00dummytest.db"};
+        AstTestMain.main(args);
         
         assertEquals("PROGRAM", outErrIntercept.toString().trim());
+    }
+    
+    @Test // Variable Declaration
+    public void datatypeDeclarationTest() throws RecognitionException {        
+        String[] args = new String[] {"TestGrammarPrograms/01datatypeDeclarationTest.db"};
+        AstTestMain.main(args);  
+        SampleFileWriter.assertFileContent(new File("TestGrammarPrograms/01AST_output"), outErrIntercept.toString().trim());
+    }
+    
+    @Test // Comments and block comments
+    public void commentTest() throws RecognitionException {
+        
+        String[] args = new String[] {"TestGrammarPrograms/02comments.db"};
+        AstTestMain.main(args);
+        SampleFileWriter.assertFileContent(new File("TestGrammarPrograms/02AST_output"), outErrIntercept.toString().trim());
+    }
+    
+    //TODO: replace this with a custom exception
+    @Test  (expected=RuntimeException.class)  // Block comments must not nest!
+    public void nestedCommentTest() throws RecognitionException {
+        String[] args = new String[] {"TestGrammarPrograms/03nestedComments.db"};
+        AstTestMain.main(args);
+    }
+    
+    //TODO: replace this with a custom exception
+    @Test  (expected=RuntimeException.class)  // Block comments must match up
+    public void missingEndComment() throws RecognitionException {
+        String[] args = new String[] {"TestGrammarPrograms/03missingEndComment.db"};
+        AstTestMain.main(args);
+    }
+    
+    //TODO: replace this with a custom exception
+    @Test  (expected=RuntimeException.class)  // Block comments must match up
+    public void missingStartComment() throws RecognitionException {
+        String[] args = new String[] {"TestGrammarPrograms/03missingStartComment.db"};
+        AstTestMain.main(args);
+    }
+    
+    @Test // Reals
+    public void realsTest() throws RecognitionException {        
+        String[] args = new String[] {"TestGrammarPrograms/04Reals.db"};
+        AstTestMain.main(args);  
+        SampleFileWriter.assertFileContent(new File("TestGrammarPrograms/04AST_output"), outErrIntercept.toString().trim());
     }
     
 }
