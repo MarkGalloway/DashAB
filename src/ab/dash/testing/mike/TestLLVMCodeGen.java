b5f619a52a812b7b11069945f7148f77e2bee78b
Mike Feist <mdfeist@ualberta.ca>
2014-10-31 19:37:22 -0600

    Remove unused imports.

--- src/ab/dash/testing/mike/TestLLVMCodeGen.java
+++ src/ab/dash/testing/mike/TestLLVMCodeGen.java
@@ -6,36 +6,23 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 import org.antlr.runtime.ANTLRFileStream;
 import org.antlr.runtime.CharStream;
-import org.antlr.runtime.CommonToken;
-import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
 import org.antlr.runtime.TokenRewriteStream;
-import org.antlr.runtime.tree.CommonTreeAdaptor;
 import org.antlr.runtime.tree.CommonTreeNodeStream;
-import org.antlr.runtime.tree.TreeVisitor;
-import org.antlr.runtime.tree.TreeVisitorAction;
-import org.antlr.stringtemplate.StringTemplate;
 import org.antlr.stringtemplate.StringTemplateGroup;
-import org.antlr.stringtemplate.language.DefaultTemplateLexer;
-
 import ab.dash.DashLexer;
 import ab.dash.DashParser;
 import ab.dash.Def;
 import ab.dash.DefineTupleTypes;
-import ab.dash.LLVMCodeGenerator;
 import ab.dash.LLVMIRGenerator;
 import ab.dash.Types;
 import ab.dash.ast.DashAST;
 import ab.dash.ast.SymbolTable;
-import ab.dash.exceptions.LexerException;
-import ab.dash.exceptions.ParserException;
 
 
 public class TestLLVMCodeGen {
