dd33774ad53e8ee546c1cd2e8b0da6d7bed5cda9
Mike Feist <mdfeist@ualberta.ca>
2014-11-20 20:08:06 -0700

    Add Interval Type.

---
+++ src/ab/dash/ast/IntervalType.java
@@ -0,0 +1,12 @@
+package ab.dash.ast;
+
+public class IntervalType extends Symbol implements Type {
+	public int lower;
+	public int upper;
+    public IntervalType(int lower, int upper) {
+        super("interval");
+        this.lower = lower;
+        this.upper = upper;
+    }
+    public int getTypeIndex() { return 0; }
+}
