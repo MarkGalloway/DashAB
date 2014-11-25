ebf5b7e94c7dbb80b06642bbe385ecbcc83da5cd
Mike Feist <mdfeist@ualberta.ca>
2014-11-25 10:21:20 -0700

    Remove checks for different sized vectors. Add function to check for different sized vectors to be added during compile.

--- runtime/vector.c
+++ runtime/vector.c
@@ -186,46 +186,31 @@ void bool_VectorNot(struct Vector* out, struct Vector* lhs) {
 		out_data[i] = (!lhs_data[i]) & 1;
 }
 
-int bool_VectorOrVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
+void bool_VectorOrVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
 	int8_t *out_data = (int8_t*) out->data;
 	int8_t *lhs_data = (int8_t*) lhs->data;
 	int8_t *rhs_data = (int8_t*) rhs->data;
-
-	if (lhs->size != rhs->size)
-		return 1;
 	
 	for (int i = 0; i < out->size; i++)
 		out_data[i] = (lhs_data[i] || rhs_data[i]) & 1;
-
-	return 0;
 }
 
-int bool_VectorXOrVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
+void bool_VectorXOrVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
 	int8_t *out_data = (int8_t*) out->data;
 	int8_t *lhs_data = (int8_t*) lhs->data;
 	int8_t *rhs_data = (int8_t*) rhs->data;
-
-	if (lhs->size != rhs->size)
-		return 1;
 	
 	for (int i = 0; i < out->size; i++)
 		out_data[i] = (lhs_data[i] ^ rhs_data[i]) & 1; 
-
-	return 0;
 }
 
-int bool_VectorAndVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
+void bool_VectorAndVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
 	int8_t *out_data = (int8_t*) out->data;
 	int8_t *lhs_data = (int8_t*) lhs->data;
 	int8_t *rhs_data = (int8_t*) rhs->data;
-
-	if (lhs->size != rhs->size)
-		return 1;
 	
 	for (int i = 0; i < out->size; i++)
 		out_data[i] = (lhs_data[i] && rhs_data[i]) & 1;
-
-	return 0;
 }
 
 void bool_VectorOrScalar(struct Vector* out, struct Vector* lhs, int8_t rhs) {
@@ -398,88 +383,58 @@ void int_assignVector(struct Vector* lhs, struct Vector* rhs) {
 		lhs_data[i] = rhs_data[i];
 }
 
-int int_VectorAddVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
+void int_VectorAddVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
 	int32_t *out_data = (int32_t*) out->data;	
 	int32_t *lhs_data = (int32_t*) lhs->data;
 	int32_t *rhs_data = (int32_t*) rhs->data;
 
-	if (lhs->size != rhs->size)
-		return 1;
-	
 	for (int i = 0; i < lhs->size; i++)
 		out_data[i] = lhs_data[i] + rhs_data[i];
-
-	return 0;
 }
 
-int int_VectorSubtractVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
+void int_VectorSubtractVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
 	int32_t *out_data = (int32_t*) out->data;	
 	int32_t *lhs_data = (int32_t*) lhs->data;
 	int32_t *rhs_data = (int32_t*) rhs->data;
-
-	if (lhs->size != rhs->size)
-		return 1;
 	
 	for (int i = 0; i < lhs->size; i++)
 		out_data[i] = lhs_data[i] - rhs_data[i];
-
-	return 0;
 }
 
-int int_VectorMultiplyVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
+void int_VectorMultiplyVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
 	int32_t *out_data = (int32_t*) out->data;	
 	int32_t *lhs_data = (int32_t*) lhs->data;
 	int32_t *rhs_data = (int32_t*) rhs->data;
-
-	if (lhs->size != rhs->size)
-		return 1;
 	
 	for (int i = 0; i < lhs->size; i++)
 		out_data[i] = lhs_data[i] * rhs_data[i];
-
-	return 0;
 }
 
-int int_VectorDivideVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
+void int_VectorDivideVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
 	int32_t *out_data = (int32_t*) out->data;	
 	int32_t *lhs_data = (int32_t*) lhs->data;
 	int32_t *rhs_data = (int32_t*) rhs->data;
-
-	if (lhs->size != rhs->size)
-		return 1;
 	
 	for (int i = 0; i < lhs->size; i++)
 		out_data[i] = lhs_data[i] / rhs_data[i];
-
-	return 0;
 }
 
-int int_VectorModulusVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
+void int_VectorModulusVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
 	int32_t *out_data = (int32_t*) out->data;	
 	int32_t *lhs_data = (int32_t*) lhs->data;
 	int32_t *rhs_data = (int32_t*) rhs->data;
-
-	if (lhs->size != rhs->size)
-		return 1;
 	
 	for (int i = 0; i < lhs->size; i++)
 		out_data[i] = lhs_data[i] % rhs_data[i];
-
-	return 0;
 }
 
-int int_VectorPowerVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
+void int_VectorPowerVector(struct Vector* out, struct Vector* lhs, struct Vector* rhs) {
 	int32_t *out_data = (int32_t*) out->data;	
 	int32_t *lhs_data = (int32_t*) lhs->data;
 	int32_t *rhs_data = (int32_t*) rhs->data;
-
-	if (lhs->size != rhs->size)
-		return 1;
 	
 	for (int i = 0; i < lhs->size; i++)
 		out_data[i] = powi(lhs_data[i], rhs_data[i]);
-
-	return 0;
 }
 
 void int_VectorUniaryMinus(struct Vector* out, struct Vector* lhs) {
@@ -490,18 +445,13 @@ void int_VectorUniaryMinus(struct Vector* out, struct Vector* lhs) {
 		out_data[i] = -lhs_data[i];
 }
 
-int int_VectorDot(int32_t *result, struct Vector* lhs, struct Vector* rhs) {
+void int_VectorDot(int32_t *result, struct Vector* lhs, struct Vector* rhs) {
 	int32_t *lhs_data = (int32_t*) lhs->data;
 	int32_t *rhs_data = (int32_t*) rhs->data;
 
-	if (lhs->size != rhs->size)
-		return 1;
-	
 	*result = 0;
 	for (int i = 0; i < lhs->size; i++)
 		*result += lhs_data[i]*rhs_data[i];
-
-	return 0;
 }
 
 int int_VectorEq(struct Vector* lhs, struct Vector* rhs) {
@@ -552,6 +502,10 @@ struct Vector* allocVector() {
 
 int32_t getVectorSize(struct Vector* vector) {
 	return vector->size;
+}
+
+int areVectorsOfSameLength(struct Vector* op1, struct Vector* op2) {
+	return op1->size == op2->size;
 }
 
 void releaseVector(struct Vector* vector) {
