10e5a850e4b420da7bdf249d3eefd56dac8688c8
E. Jed Barlow <jed@jedbarlow.ca>
2014-11-30 08:51:01 -0700

    add scalars to test for vector operation xor

--- src/ab/dash/testing/TestLLVM.java
+++ src/ab/dash/testing/TestLLVM.java
@@ -1197,6 +1197,16 @@ public class TestLLVM extends BaseTest {
         sb.append("T\n");
         sb.append("F\n");
 
+        sb.append("T\n");
+        sb.append("F\n");
+        sb.append("T\n");
+        sb.append("F\n");
+
+        sb.append("F\n");
+        sb.append("T\n");
+        sb.append("F\n");
+        sb.append("T\n");
+
         assertEquals(sb.toString().trim(), outErrIntercept.toString().trim());
     }
     
