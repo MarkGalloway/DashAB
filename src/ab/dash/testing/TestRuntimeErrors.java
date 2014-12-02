8bf0060116d227ac2cdeb0893a405ae3faec15cf
E. Jed Barlow <jed@jedbarlow.ca>
2014-12-02 13:03:29 -0700

    add test for runtime error for dividing vectors with different lengths

--- src/ab/dash/testing/TestRuntimeErrors.java
+++ src/ab/dash/testing/TestRuntimeErrors.java
@@ -70,4 +70,15 @@ public class TestRuntimeErrors extends BaseTest {
 
         assertEquals(sb.toString().trim(), outErrIntercept.toString().trim());
     }
+
+    @Test
+    public void divideDifferentLengthVectors() throws IOException, RecognitionException, LexerException, ParserException, SymbolTableException, InterruptedException {
+        String[] args = new String[] {"TestRuntimeErrors/004DivideDifferentLengthVectors/divideDifferentLengthVectors.ds"};
+        Runner.llvmMain(args);
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("RuntimeError: Vectors are not of same length.\n");
+
+        assertEquals(sb.toString().trim(), outErrIntercept.toString().trim());
+    }
 }
